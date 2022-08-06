8e0081bf552040ccedea8bc2611c3e55ee410d1c
huiwonjeong <hjeong303@gatech.edu>
2022-08-06 02:12:51 -0700

    custom css

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,91 +5,13 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 dark xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      {/* 이런식으로 상위 html에 dark라는 className을 추가하면 그 아래 모든 아이들이 dark모드 활성화 된다 따라서 <html>나 <body>에 추가하면 활성화! */}
-      <div className="bg-white p-6  rounded-3xl shadow-2xl flex flex-col justify-around dark:bg-black">
-        <span className="font-semibold text-3xl dark:text-white">
-          Select Item
-        </span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
-          <span className="font-semibold dark:text-white">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
-          <span className="font-semibold dark:text-white">$19</span>
-        </div>
-        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$10</span>
-        </div>
-        <button
-          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/3 mx-auto block
-        hover:bg-teal-500 dark:hover:bg-black dark:hover:text-white hover:text-black active:bg-yellow-500"
-        >
-          Checkout
-        </button>
-      </div>
-      <div className="bg-white rounded-2xl shadow-2xl overflow-hidden">
-        <div className="bg-blue-500 p-6 pb-14 xl:pb-40 landscape:bg-teal-500 portrait:bg-indigo-500">
-          <span className="text-white text-2xl">Profile</span>
-        </div>
-        <div className="rounded-3xl p-6 relative -top-5 bg-white">
-          <div className="flex relative -top-16 items-end justify-between">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$340</span>
-            </div>
-          </div>
-          <div className="relative flex flex-col items-center -mt-10 -mb-5">
-            <span>Tony Molloy</span>
-            <span>New York, USA</span>
-          </div>
-        </div>
-      </div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl lg:col-span-2 xl:col-span-1 ">
-        <div className="flex justify-between items-center mb-5">
-          <span>⬅</span>
-          <div className="space-x-3">
-            <span>⭐ 4.9</span>
-            <span className="shadow-2xl p-2">❤</span>
-          </div>
-        </div>
-        <div className="bg-zinc-400 h-72 mb-5" />
-        <div className="flex flex-col">
-          <span className="font-medium text-xl">Swoon Lounge</span>
-          <span className="text-s text-gray-500">Chair</span>
-          <div className="mt-3 mb-5 flex justify-between items-center">
-            <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
-              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
-              <button className="w-5 h-5 rounded-full bg-teal-500  focus:ring-2 ring-offset-2 ring-teal-500 transition" />
-            </div>
-            <div className="flex items-center space-x-4">
-              <button className="p-1.5 text-lg bg-blue-200 flex justify-center items-center aspect-square w-10 rounded-lg">
-                -
-              </button>
-              <span className="text-lg font-medium">1</span>
-              <button className="p-1.5 text-lg bg-blue-200 flex justify-center items-center aspect-square w-10 rounded-lg">
-                +
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <span className="font-medium text-2xl">$450</span>
-            <button className="bg-blue-500 text-center text-white rounded-xl py-2 px-8 text-sm">
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <h2 className="text-[100px]">Hello</h2>
+      <div className="bg-[url('/vercel.svg')] bg-cover h-40 w-100"></div>
+      <h2 className="text-[#ff3232] text-[50px]">Color</h2>
     </div>
   );
+  // Just intime compiler 덕분에 tailwindCss의 규정에 벗어나서 사용할수 있다.
 };
 
 export default Home;
