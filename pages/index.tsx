f63db5d0cbd492f87d45f9570ceb0d669d909b1a
huiwonjeong <hjeong303@gatech.edu>
2022-08-07 02:12:54 -0700

    COMMIT

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h2 className="text-[100px]">Hello</h2>
-      <div className="bg-[url('/vercel.svg')] bg-cover h-40 w-100"></div>
-      <h2 className="text-[#ff3232] text-[50px]">Color</h2>
-    </div>
-  );
-  // Just intime compiler 덕분에 tailwindCss의 규정에 벗어나서 사용할수 있다.
+  return <div></div>;
 };
 
 export default Home;
